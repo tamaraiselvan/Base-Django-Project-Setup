796a73c2d39fddea951ce557cee30f0234b91a6a
TS Tamarai Selvan <tamaraiselvan.official@gmail.com>
2023-06-26 08:42:48 +0530

    Minor UI Changes.
    There is Minor UI changes. Eye symbol in password fields.

--- firstapp/forms.py
+++ firstapp/forms.py
@@ -9,8 +9,8 @@ class SignUpForm(UserCreationForm):
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Enter last name'}), required=True)
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder': 'Enter Email address'}), required=True)
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Enter username'}), required=True)
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder': 'Enter Password'}), required=True)
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder': 'ReEnter Password'}), required=True)
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder': 'Enter Password','aria-describedby':"password1-visible"}), required=True)
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder': 'ReEnter Password','aria-describedby':"reenter-password-visible"}), required=True)
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
